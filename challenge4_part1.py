684be498d180c1341a5c703046e0833492f62718
Patrick Joos <patrick.joos@ntb.ch>
2019-12-04 11:06:35 +0100

    Change: Finished day 4 part 1

---
+++ challenge4_part1.py
@@ -0,0 +1,30 @@
+
+
+def verify_pw(pw: int) -> bool:
+    # pwString =
+    # a = [x for x in pwString]
+    elements: [int] = list(map(lambda digit: int(digit), str(pw)))
+
+    double: bool = False
+    ascending: bool = True
+    for i in range(0, len(elements)-1, 1):
+        if elements[i] == elements[i+1]:
+            double = True
+        if elements[i] > elements[i+1]:
+            ascending = False
+
+    return double and ascending
+
+def count_valid_pws(start: int, end: int) -> int:
+    counter: int = 0
+    for i in range(start, end, 1):
+        if verify_pw(i):
+            counter += 1
+    return counter
+
+
+print("111111: [true|" + str(verify_pw(111111)) + "]")
+print("111111: [false|" + str(verify_pw(223450)) + "]")
+print("111111: [false|" + str(verify_pw(123789)) + "]")
+
+print("Valid pws: " + str(count_valid_pws(240920, 789857)))
