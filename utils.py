8d3b0f6f69714523473494f3865e4a1bbf552ec5
Patrick Joos <patrick.joos@ntb.ch>
2019-12-12 08:15:26 +0100

    Change: finished challenge 12 part 1

--- utils.py
+++ utils.py
@@ -9,10 +9,35 @@ def length(vec: (int, int)) -> float:
     return math.sqrt(math.pow(vec[0], 2) + math.pow(vec[1], 2))
 
 
-def add(a: (int, int), b: (int, int)) -> (int, int):
-    return (a[0] + b[0], a[1] + b[1])
+def sign(x):
+    if x > 0:
+        return 1.
+    elif x < 0:
+        return -1.
+    elif x == 0:
+        return 0.
+    else:
+        return x
+
+
+def add(a, b):
+    arr = []
+    for x in range(0, len(a), 1):
+        arr.append(a[x] + b[x])
+    return tuple(arr)
+
+
+def subtract(a, b):
+    arr = []
+    for x in range(0, len(a), 1):
+        arr.append(a[x] - b[x])
+    return tuple(arr)
 
 
 def normalize(vec: (int, int)) -> (int, int):
     magn = length(vec)
     return (vec[0] / magn, vec[1] / magn)
+
+
+def absVec(a: ()) -> ():
+    return tuple([abs(x) for x in a])
